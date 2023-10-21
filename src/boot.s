64bc7b31a2728639281cc24a8ac5e7ba06a9659f
xfxpositions <xfxpositions@gmail.com>
2023-10-22 02:09:27 +0300

    multiboot header checksum fixed

--- src/boot.s
+++ src/boot.s
@@ -8,4 +8,4 @@ align 4 ; align 32-bit
 multiboot_header:
     dd 0x1BADB002 ; magic number
     dd 0x0 ; flags, it's empty by default
-    dd - (0xe85250d6 + 0) ; checksum for multiboot header
+    dd - (0x1BADB002 + 0) ; checksum for multiboot header
