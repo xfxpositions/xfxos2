882feddca05b2f856d0914ac2cc023029d9e0cd3
xfxpositions <xfxpositions@gmail.com>
2023-10-22 02:08:55 +0300

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
-zig build
-nasm src/boot.s -f elf32 -o build/boot.o
-ld -m elf_i386 -Ttext 0x100000 -o build/kernel.bin build/boot.o build/kernel.o
-sudo grub-mkrescue -o build/os.iso grub/grub.cfg build/kernel.bin
-qemu-system-i386 build/os.iso
+build=build # build dir
+isoname=xfxos.iso # isoname
+mkdir -p $build # create build dir
+
+zig build # build with build.zig file 
+nasm src/boot.s -f elf32 -o build/boot.o # compile multiboot header
+ld -m elf_i386 -T linker.ld -o build/isofiles/boot/kernel.bin build/boot.o build/kernel.o # link the kernel
+
+d=build/isofiles/boot/grub
+mkdir -p "$d" && cp grub.cfg "$d"
+
+sudo grub-mkrescue -o build/os.iso build/isofiles # create iso
+qemu-system-i386 build/os.iso # run with qemu
