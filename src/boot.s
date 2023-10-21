c0afe3886cb1d00290a4522c56baad9565a66303
xfxpositions <xfxpositions@gmail.com>
2023-10-21 19:02:17 +0300

    boot.s added, multiboot header

---
+++ src/boot.s
@@ -0,0 +1,11 @@
+; For documentation
+; https://www.gnu.org/software/grub/manual/multiboot/multiboot.html
+
+; Multiboot header
+section .multiboot
+align 4 ; align 32-bit
+
+multiboot_header:
+    dd 0x1BADB002 ; magic number
+    dd 0x0 ; flags, it's empty by default
+    dd - (0xe85250d6 + 0) ; checksum for multiboot header
