ce912e765dc3e4e2d19ec0b6cd054efc7303ef21
xfxpositions <xfxpositions@gmail.com>
2023-10-27 15:40:11 +0300

    println tests (not working)

--- src/main.zig
+++ src/main.zig
@@ -4,11 +4,28 @@ export fn _start() noreturn {
     var printer = tty.Tty.init();
     printer.clear();
 
-    var i: u8 = 0;
-    while (i < 10) {
-        printer.put_chr(1, i, tty.VgaEntry{ .char = 'A' + i, .bg = tty.Color.RED });
-        i = i + 1;
-    }
+    // while (i < 10) {
+    //     printer.put_chr(1, i, tty.VgaEntry{ .char = 'A' + i, .bg = tty.Color.RED });
+    //     i = i + 1;
+    // }
+    // printer.println("Hello\n");
+    // var x: u8 = 0;
+    // for ("Kirwe") |chr| {
+    //     printer.put_chr(x, 3, tty.VgaEntry{ .char = chr });
+    //     x += 1;
+    // }
+    var str = "Kirwe";
+    _ = str;
 
+    // var i: usize = 0;
+    // var x_1: u8 = 0;
+    // while (i < str.len) : (i += 1) {
+    //     const chr: u8 = str[i];
+    //     printer.put_chr(x_1, 5, tty.VgaEntry{ .char = chr });
+    //     x_1 += 1;
+    // }
+
+    printer.put_chr(10, 10, tty.VgaEntry{ .char = 72 });
+    printer.println("Hello!");
     while (true) {}
 }
