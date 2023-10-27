b2e5a3db2de06d84bf1e9365219b919403e2afda
xfxpositions <xfxpositions@gmail.com>
2023-10-27 14:03:12 +0300

    ptr_chr test

--- src/main.zig
+++ src/main.zig
@@ -3,8 +3,12 @@ const tty = @import("tty.zig");
 export fn _start() noreturn {
     var printer = tty.Tty.init();
     printer.clear();
-    for (0..10) |i| {
-        printer.put_chr(1, i, tty.VgaEntry{ .char = 'A', .bg = tty.Color.RED, .fg = tty.Color.WHITE });
+
+    var i: u8 = 0;
+    while (i < 10) {
+        printer.put_chr(1, i, tty.VgaEntry{ .char = 'A' + i, .bg = tty.Color.RED });
+        i = i + 1;
     }
+
     while (true) {}
 }
