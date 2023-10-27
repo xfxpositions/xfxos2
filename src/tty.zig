ff96ca4202fe21d4ef7f89f4c23532a2a85b90f2
xfxpositions <xfxpositions@gmail.com>
2023-10-27 15:40:32 +0300

    printer Cursor and println added

--- src/tty.zig
+++ src/tty.zig
@@ -15,15 +15,29 @@ pub const VgaEntry = packed struct {
     fg: Color = Color.WHITE, // background color
 };
 
+///
+/// Cursor of the Tty
+/// it has position, and blink
+///
+pub const Cursor = struct {
+    x: u16,
+    y: u16,
+    blink: bool,
+    pub fn init() Cursor {
+        return Cursor{ .x = 0, .y = 0, .blink = false };
+    }
+};
+
 pub const Tty = struct {
     buffer: *volatile [VGA_SIZE]VgaEntry,
+    cursor: Cursor,
     pub fn init() Tty {
         var buffer: *volatile [25 * 80]VgaEntry = @ptrFromInt(VGA_BUFFER);
 
-        return Tty{ .buffer = buffer };
+        return Tty{ .buffer = buffer, .cursor = Cursor.init() };
     }
     pub fn put_chr(self: Tty, x: usize, y: usize, entry: VgaEntry) void {
-        self.buffer[x * y] = entry;
+        self.buffer[y * VGA_WIDTH + x] = entry;
     }
     /// Clears the vga text buffer
     pub fn clear(self: Tty) void {
@@ -31,8 +45,18 @@ pub const Tty = struct {
             self.buffer[i] = VgaEntry{ .char = ' ', .bg = Color.BLACK, .fg = Color.BLACK };
         }
     }
-    pub fn println(self: Tty, text: []u8) void {
-        _ = text;
-        _ = self;
+    pub fn println(self: *Tty, str: []const u8) void {
+        for (str) |chr| {
+            // Add newline and update cursor
+            if (chr == '\n') {
+                self.cursor.y += 1;
+                continue;
+            }
+
+            self.put_chr(self.cursor.x, self.cursor.y, VgaEntry{ .char = chr });
+            self.cursor.x += 1;
+        }
+        // Add newline and update cursor
+        self.cursor.y += 1;
     }
 };
